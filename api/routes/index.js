65fe5ef319dd3d518f7babc2de164393e0b6dc04
Adriano Barbosa <adriano@adrianob.com.br>
2021-07-21 22:54:53 -0300

    keep heading

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const movies = require('./moviesRoutes')
 
 module.exports = app => {
     app.use(express.urlencoded({
         extended: true
     }))
     app.use(express.json())
+    app.use(movies)
     app.get("/",(req,res)=>res.status(200).json({message:'It works!'}))
 }
---
+++ api/routes/moviesRoutes.js
@@ -0,0 +1,7 @@
+const { Router } = require('express')
+
+const router = Router()
+
+router.get('/videos',(req,res)=>res.json({message:'ok falta implementar'}))
+
+module.exports = router
