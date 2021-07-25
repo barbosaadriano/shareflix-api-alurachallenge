8ff45593f0de29dc694c3b1268d388e1175f6ba2
Adriano Barbosa <adriano@adrianob.com.br>
2021-07-25 10:50:12 -0300

    adição da documentação

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const movies = require('./moviesRoutes')
+const swaggerUi = require('swagger-ui-express')
+const openApiDocumentation = require('../../docs/openapi.json')
 
 module.exports = app => {
     app.use(express.urlencoded({
@@ -8,4 +10,5 @@ module.exports = app => {
     app.use(express.json())
     app.use(movies)
     app.get("/",(req,res)=>res.status(200).json({message:'It works!'}))
+    app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(openApiDocumentation))
 }
