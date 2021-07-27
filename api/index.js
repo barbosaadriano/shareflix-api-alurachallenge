e3c57d77ee3742c0bc5a249a9c43b11c80599862
Adriano Barbosa <adriano@adrianob.com.br>
2021-07-26 23:23:37 -0300

    teste integração

--- api/index.js
+++ api/index.js
@@ -3,10 +3,13 @@ const routes = require('./routes')
 const cors = require('cors')
 
 const app = express()
-const port = 3000
+const port = 8080
 
+app.use(cors())
 routes(app)
+
 if (require.main === module) {
     app.listen(port,()=>console.log(`Server is running at ${port}`))
 }
+
 module.exports = app
